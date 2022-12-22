6ecc280700c03853e854aa001e8b3a2f21ec27c9
Lei Ni <leni@microsoft.com>
2022-12-22 17:36:57 +0800

    add basic files

---
+++ src/main.ts
@@ -0,0 +1,53 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import {
+  AutoRestExtension,
+  AutorestExtensionHost,
+  startSession,
+} from "@autorest/extension-base";
+import { CodeModel, codeModelSchema } from "@autorest/codemodel";
+
+export async function processRequest(host: AutorestExtensionHost) {
+  try {
+    const session = await startSession<CodeModel>(
+      host,
+      undefined,
+      codeModelSchema
+    );
+    const start = Date.now();
+
+    // for (const definition of getProviderDefinitions(session.model, host)) {
+    //   const { namespace, apiVersion } = definition;
+    //   const types = generateTypes(host, definition);
+
+    //   const outFolder = `${namespace}/${apiVersion}`.toLowerCase();
+
+    //   // write types.json
+    //   host.writeFile({
+    //     filename: `${outFolder}/types.json`,
+    //     content: writeJson(types),
+    //   });
+
+    //   // writer types.md
+    //   host.writeFile({
+    //     filename: `${outFolder}/types.md`,
+    //     content: writeMarkdown(types, `${namespace} @ ${apiVersion}`),
+    //   });
+    // }
+
+    session.info(`autorest.bicep took ${Date.now() - start}ms`);
+  } catch (err) {
+    console.error("An error was encountered while handling a request:", err);
+    throw err;
+  }
+}
+
+async function main() {
+  const pluginHost = new AutoRestExtension();
+  pluginHost.add("azureresourceschema", processRequest);
+  await pluginHost.run();
+}
+
+// eslint-disable-next-line jest/require-hook
+main();
